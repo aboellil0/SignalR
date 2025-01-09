140f368eadde490afc9478261a9f3fb8b33a3588
Mohamed Aboellil <mohamed.aboellil0@gmail.com>
2025-01-09 19:26:39 +0200

    Add project files.

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Models;
+using System;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly SignalRContext _context;
+
+        public ChatHub(SignalRContext context)
+        {
+            this._context = context;
+        }
+        [HubMethodName("SendMessege")]
+        public void Aboellil(string name, string message)
+        {
+            //save in DB 
+            _context.Chats.Add(new Chat { UserName = name, Messege = message });
+            _context.SaveChanges();
+
+            //broadcasting to all online clints
+            
+            Clients.All.SendAsync("NewMessege", name, message).Wait();
+        }
+        public void GetAll()
+        {
+
+            var chat = _context.Chats.ToList();
+            foreach (var chatItem in chat)
+            {
+                Clients.All.SendAsync("GetAllMesseges", chatItem.UserName, chatItem.Messege);
+            }
+        }
+        [HubMethodName("joingroup")]
+        public void JoinToGroup(string groupname,string name)
+        {
+            try
+            {
+                //add to group
+                Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+
+                //boadcasting 
+                Clients.OthersInGroup(groupname).SendAsync("groupsend", name, groupname);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in JoinToGroup: {ex.Message}");
+            }
+        }
+
+        public void SendToGroup()
+        {
+
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            string conId = Context.ConnectionId;
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
---
+++ SignalR/wwwroot/js/Chat.js
@@ -0,0 +1,71 @@
+﻿console.log("Aboellil is man");
+// Define Connection
+var con = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+
+// Start Connection
+con.start()
+    .then(() => {
+        console.log("SignalR connection established.");
+        GetAll(); // Call GetAll after the connection is successfully established
+    })
+    .catch(err => console.error(err.toString()));
+
+// Define Callback function
+con.on("GetAllMesseges", (name, messege) => {
+    $("#messeges").append("<li>" + name + " : " + messege + "</li>");
+});
+
+con.on("NewMessege", function (name, messege) {
+    $("#messeges").append("<li>" + name + " : " + messege + "</li>");
+});
+
+con.on("groupsend", function (name, groupname) {
+    console.log("Received groupsend:", { name, groupname });
+    $("#messeges").append("<li>" + name + " ::::: " + groupname + "</li>");
+});
+
+
+
+
+// Function to consume service
+function SendM() {
+
+    var SinderName = $("#name").val();
+    var SinderMessege = $("#messege").val();
+
+    con.invoke("SendMessege", SinderName, SinderMessege).catch(err => console.error(err.toString()));
+
+    $("#messege").val("");
+}
+
+function GetAll() {
+    con.invoke("GetAll").catch(err => console.error(err.toString()));
+}
+console.log("Script loaded!");
+
+function JoinGroup() {
+    var SinderName = $("#name").val();
+    var GroupName = $("#groupname").val();
+
+    console.log("Joining:", GroupName, "with name:", SinderName); // Debug log
+
+    con.invoke("joingroup", GroupName, SinderName);
+}
+
+
+$("#SendBtn").on("click", () => {
+    SendM();
+});
+
+$("#messege").on("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        SendM();
+    }
+})
+
+
+$("#joinbtn").on("click", () => {
+    JoinGroup();
+});
+
